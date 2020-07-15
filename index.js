95625858d219e0b40f19049143e4ef9483f26a4f
Hebilicious <xsh4k3@gmail.com>
2020-07-15 08:35:01 +0100

    Add onlyCompileBundledFiles

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const { resolve } = require('path');
+const { resolve } = require("path")
 
-function TypescriptPlugin (api) {
+function TypescriptPlugin(api) {
   api.chainWebpack((config) => {
     config.module
-      .rule('typescript')
-        .test(/\.tsx?$/)
-        .use()
-          .loader('ts-loader')
-          .options({ appendTsSuffixTo: [/\.vue$/] });
+      .rule("typescript")
+      .test(/\.tsx?$/)
+      .use()
+      .loader("ts-loader")
+      .options({ onlyCompileBundledFiles: true, appendTsSuffixTo: [/\.vue$/] })
 
-    config.resolve.extensions
-      .add('.ts');
+    config.resolve.extensions.add(".ts")
 
-    config.resolve.alias
-      .set('vue$', resolve(process.cwd(), './node_modules/vue/dist/vue.esm.js'));
-  });
+    config.resolve.alias.set("vue$", resolve(process.cwd(), "./node_modules/vue/dist/vue.esm.js"))
+  })
 }
 
-module.exports = TypescriptPlugin;
+module.exports = TypescriptPlugin
